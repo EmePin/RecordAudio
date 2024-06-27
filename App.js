3450366d57bfbd7ded1c426c5eae33239266f62c
EmePin <aimeejahdai@gmail.com>
2024-06-27 17:25:22 -0600

    chore: Update npm dependencies
    This commit updates the npm dependencies in the `package.json` file.

--- App.js
+++ App.js
@@ -1,11 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
+
+  async function startRecording() {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        const { recording } = await Audio.Recording.createAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+        setRecording(recording);
+      }
+    } catch (err) {}
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+    let allRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    allRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    setRecordings(allRecordings);
+  }
+
+  async function playPredefinedSound() {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('./assets/sound.mp3'));
+      await soundObject.playAsync();
+    } catch (error) {
+      console.log('Error playing sound:', error);
+    }
+  }
+
+  function getDurationFormatted(milliseconds) {
+    const minutes = milliseconds / 1000 / 60;
+    const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
+    return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>
+            Recording #{index + 1} | {recordingLine.duration}
+          </Text>
+          <TouchableOpacity onPress={() => recordingLine.sound.replayAsync()} style={styles.button}>
+            <Text style={styles.buttonText}>Play</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    });
+  }
+
+  function clearRecordings() {
+    setRecordings([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.soundButton} onPress={playPredefinedSound}>
+        <Text style={styles.buttonText}>Play Sound</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.mainButton} onPress={recording ? stopRecording : startRecording}>
+        <Text style={styles.buttonText}>{recording ? 'Stop Recording' : 'Start Recording'}</Text>
+      </TouchableOpacity>
+      {getRecordingLines()}
+      {recordings.length > 0 &&
+        <TouchableOpacity style={styles.clearButton} onPress={clearRecordings}>
+          <Text style={styles.buttonText}>Clear Recordings</Text>
+        </TouchableOpacity>
+      }
     </View>
   );
 }
@@ -16,5 +94,50 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+  fill: {
+    flex: 1,
+    margin: 15,
+  },
+  mainButton: {
+    backgroundColor: '#007BFF',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  soundButton: {
+    backgroundColor: '#28a745',
+    paddingVertical: 15,
+    margin:20,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  clearButton: {
+    backgroundColor: '#DC3545',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginHorizontal: 10,
   },
 });
